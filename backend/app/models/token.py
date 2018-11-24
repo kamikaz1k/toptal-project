7bc8600c28585040f009466f2ee7c8f144e09705
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-24 15:10:07 -0500

    added user_id FK constraint to Token model

--- backend/app/models/token.py
+++ backend/app/models/token.py
@@ -1,10 +1,13 @@
 from app.database import db
 
+from app.models.user import User
+from sqlalchemy.schema import ForeignKey
+
 
 class Token(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
     jwt_token = db.Column(db.String(225), nullable=False, index=True, unique=True)
     created_on = db.Column(db.TIMESTAMP, nullable=True)
     updated_on = db.Column(db.TIMESTAMP, nullable=True)
