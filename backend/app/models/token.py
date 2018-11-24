0ff72844a1da41aa7d696e749a444f2d530fcae1
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-24 16:14:58 -0500

    Updated models to use correct updated_at setting

--- backend/app/models/token.py
+++ backend/app/models/token.py
@@ -1,6 +1,7 @@
 from app.database import db
-
 from app.models.user import User
+
+from sqlalchemy import func
 from sqlalchemy.schema import ForeignKey
 
 
@@ -9,7 +10,9 @@ class Token(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
     jwt_token = db.Column(db.String(225), nullable=False, index=True, unique=True)
-    created_on = db.Column(db.TIMESTAMP, nullable=True)
-    updated_on = db.Column(db.TIMESTAMP, nullable=True)
+
+    updated_at = db.Column(db.TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now)
+    created_at = db.Column(db.TIMESTAMP, nullable=False, server_default=func.now())
+
     expires_on = db.Column(db.TIMESTAMP, nullable=False)
     revoked_on = db.Column(db.TIMESTAMP, nullable=True)
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,3 +1,5 @@
+from sqlalchemy import func
+
 from app.database import db
 
 
@@ -7,3 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(80), nullable=False)
+
+    updated_at = db.Column(db.TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now)
+    created_at = db.Column(db.TIMESTAMP, nullable=False, server_default=func.now())
+    deleted_at = db.Column(db.TIMESTAMP, nullable=True)
