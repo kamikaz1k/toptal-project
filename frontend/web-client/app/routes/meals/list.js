1a3673aa3933b24e930005e8e22fde9cc868e1b7
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-27 09:29:54 -0500

    added query params to list view and added link to search page

--- frontend/web-client/app/routes/meals/list.js
+++ frontend/web-client/app/routes/meals/list.js
@@ -4,12 +4,29 @@ import { inject } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
 
+  queryParams: {
+    p: { refreshModel: true },
+    startDate: {},
+    endDate: {},
+    startTime: {},
+    endTime: {},
+  },
+
   store: inject(),
 
-  model() {
+  model(params) {
+    let queryParams = this._buildQueryOptions(params);
     return {
-      meals: this.get('store').query('meal', { p: 1 })
+      meals: this.get('store').query('meal', queryParams)
     }
+  },
+
+  _buildQueryOptions(params) {
+    let queryParams = Object.assign({}, params);
+    if (queryParams.p == undefined) {
+      queryParams.p = 1;
+    }
+    return queryParams;
   },
 
   actions: {
