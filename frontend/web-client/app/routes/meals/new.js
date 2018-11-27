60cb2bbae37dd8af688f79a67a79d0b4df4cee30
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-26 21:30:18 -0500

    Added new meal entry form and route

--- frontend/web-client/app/routes/meals/new.js
+++ frontend/web-client/app/routes/meals/new.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { inject } from '@ember/service';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
+
+  store: inject(),
+
+  actions: {
+    saveMeal(form) {
+      console.log("saveMeal", form);
+      this.get('store').createRecord('meal', {
+        text: form.get('text'),
+        calories: form.get('calories'),
+        entryDatetime: new Date(form.get('entryDatetime'))
+      }).save().then(() => {
+        this.transitionTo('meals.list');
+      }).catch(e => {
+        console.error(e);
+        alert("Something went wrong...");
+      });
+    }
+  }
 });
