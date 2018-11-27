3681656f50536ec302cc64f95e7020bfd9ec840f
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-26 23:24:37 -0500

    added meal delete feature

--- frontend/web-client/app/routes/meals/list.js
+++ frontend/web-client/app/routes/meals/list.js
@@ -10,5 +10,22 @@ export default Route.extend(AuthenticatedRouteMixin, {
     return {
       meals: this.get('store').query('meal', { p: 1 })
     }
+  },
+
+  actions: {
+    deleteMeal(mealId) {
+      console.log("delete!", mealId);
+      let meal = this.get('store').peekRecord('meal', mealId);
+      if (meal) {
+        meal.deleteRecord();
+        meal.save().then(() => {
+          console.log("deleted mealId ", mealId);
+        }).catch(e => {
+          console.error(e);
+          alert("something went wrong...");
+        })
+      }
+    }
   }
+
 });
