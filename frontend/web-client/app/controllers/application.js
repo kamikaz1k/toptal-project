0259eddde40e4cbd08f104a3492b23db017a406e
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-27 17:25:33 -0500

    added admin and user_manager role checks to app

--- frontend/web-client/app/controllers/application.js
+++ frontend/web-client/app/controllers/application.js
@@ -1,6 +1,28 @@
 import Controller from '@ember/controller';
+import { computed, get } from '@ember/object';
 import { inject } from '@ember/service';
+import jwtDecode from 'ember-jwt-decode';
 
 export default Controller.extend({
-  session: inject()
+
+  session: inject(),
+
+  isAdmin: computed('session', function() {
+    let token = this.get('session.data.authenticated.token');
+    if (token) {
+      let decoded = jwtDecode(token);
+      return !!get(decoded, 'roles.is_admin');
+    }
+    return false;
+  }),
+
+  isUserManager: computed('session', function() {
+    let token = this.get('session.data.authenticated.token');
+    if (token) {
+      let decoded = jwtDecode(token);
+      return !!get(decoded, 'roles.is_user_manager');
+    }
+    return false;
+  })
+
 });
