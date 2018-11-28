d2c154afca99b60295ef04bc3f6254c57aae2394
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-27 19:17:08 -0500

    added search redirect for meals filtering

--- frontend/web-client/app/routes/meals/search.js
+++ frontend/web-client/app/routes/meals/search.js
@@ -2,26 +2,36 @@ import moment from 'moment';
 import EmberObject from '@ember/object';
 import Route from '@ember/routing/route';
 
+const TIME_FORMAT_AM = 'hh:mm A';
+const TIME_FORMAT_ISO = 'HH:mm';
+const DATE_FORMAT = 'YYYY-MM-DD';
+
 export default Route.extend({
 
   model() {
     return EmberObject.create({
-      startDate: moment().format('MM-DD-YYYY'),
-      endDate: moment().format('MM-DD-YYYY'),
-      startTime: moment().format('hh:mm A'),
-      endTime: moment().format('hh:mm A')
+      startDate: null,
+      endDate: null,
+      startTime: moment().format(TIME_FORMAT_AM),
+      endTime: moment().format(TIME_FORMAT_AM)
     });
   },
 
   actions: {
     setSearchParams(model) {
       let props = {
-        startDate: moment(model.get('startDate'), 'MM-DD-YYYY'),
-        endDate: moment(model.get('endDate'), 'MM-DD-YYYY'),
-        startTime: moment(model.get('startTime'), 'hh:mm A'),
-        endTime: moment(model.get('endTime'), 'hh:mm A')
+        startDate: moment(model.get('startDate'), DATE_FORMAT).format(DATE_FORMAT),
+        endDate: moment(model.get('endDate'), DATE_FORMAT).format(DATE_FORMAT),
+        startTime: moment(model.get('startTime'), TIME_FORMAT_AM).format(TIME_FORMAT_ISO),
+        endTime: moment(model.get('endTime'), TIME_FORMAT_AM).format(TIME_FORMAT_ISO),
       }
+      Object.keys(props).forEach(key => {
+        if (props[key] == "Invalid date") {
+          delete props[key];
+        }
+      });
       console.log(model, props);
+      this.transitionTo('meals.list', { queryParams: props })
     }
   }
 
