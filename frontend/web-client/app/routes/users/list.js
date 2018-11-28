19af236315e09ce303e0f74cd713b91c2b59a043
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-27 23:41:00 -0500

    added pagination to user list view

--- frontend/web-client/app/routes/users/list.js
+++ frontend/web-client/app/routes/users/list.js
@@ -6,10 +6,26 @@ export default Route.extend(AuthenticatedRouteMixin, {
 
   store: inject(),
 
+  queryParams: {
+    p: { refreshModel: true }
+  },
+
   model(params) {
     return {
-      users: this.get('store').query('user', { p: 1 })
+      users: this.get('store').query('user', { p: params.p || 1 })
     }
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+
+    let page = parseInt(controller.get('p')) || 1;
+
+    if (page > 1) {
+      controller.set('previousPage', page - 1);
+    }
+    controller.set('nextPage', page + 1);
+
   },
 
   actions: {
