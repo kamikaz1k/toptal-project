8332df8cfbd95680b8ab99665a40404be387bfd7
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-11-27 23:03:19 -0500

    fixed registration flow

--- frontend/web-client/app/routes/signup.js
+++ frontend/web-client/app/routes/signup.js
@@ -9,19 +9,25 @@ export default Route.extend({
     signup(form) {
       console.log(form);
 
-      let user = this.get('store').createRecord('user', {
-        email: form.get('email'),
-        password: form.get('password'),
-        name: form.get('name')
-      });
-
-      user.save().then(response => {
+      fetch("/api/users", {
+        "headers":{
+          "content-type":"application/json"
+        },
+        "body": JSON.stringify({
+          user: {
+            email: form.get('email'),
+            password: form.get('password'),
+            name: form.get('name')
+          }
+        }),
+        "method":"POST"
+      }).then(response => {
         console.log(response);
-        alert("You have signed up successfully " + user.get('name'));
+        alert("You have signed up successfully " + form.get('name'));
         this.transitionTo('login');
       }).catch(e => {
         let err = e.errors[0];
-        let email = user.get('email');
+        let email = form.get('email');
         console.log(err);
         alert(`Looks like ${email} is already taken`);
       });
