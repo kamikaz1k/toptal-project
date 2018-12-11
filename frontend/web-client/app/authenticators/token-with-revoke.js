fcdcf4e3a3fb23b2241d9d2a9c23ba2117bdfe6e
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-12-10 20:04:34 -0500

    changed authenticator to use auto jwt invalidation

--- frontend/web-client/app/authenticators/token-with-revoke.js
+++ frontend/web-client/app/authenticators/token-with-revoke.js
@@ -1,10 +1,17 @@
-import TokenBase from 'ember-simple-auth-token/authenticators/token';
+import TokenBase from 'ember-simple-auth-token/authenticators/jwt';
+import { inject } from '@ember/service';
+
 
 export default TokenBase.extend({
-  invalidate(data) {
-    let token = data.token;
+
+  session: inject(),
+
+  invalidate() {
+    let token = this.get('session.data.authenticated.token');
     return this.makeRequest("/auth/logout", {}, {
       'Authorization': `Bearer ${token}`
+    }).finally(() => {
+      this._super(...arguments);
     });
   }
 });
